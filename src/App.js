ae1decf9ed06809bba246af9ada1f8c854fc1646
bmm228 <bmm228@pitt.edu>
2025-01-22 13:20:07 -0500

    Comment out how to reload

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <Greeting name="World" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         // Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
